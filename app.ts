9ab2400b34815b69ec00a876e0e85ad67f10b01f
Vassily Krasnov <mykrasnov@gmail.com>
2019-06-16 22:31:30 +0300

    Add enum type

--- app.ts
+++ app.ts
@@ -21,3 +21,19 @@ el.innerHTML = `Name: ${tom.name}, age: ${tom.age}`;
 let x: number = 10;
 let hello: string = 'Hello, world';
 let isValid: boolean = true;
+
+
+// Тип enum
+(() => {
+  enum Season { Winter, Spring, Summer, Autumn };
+
+  let current: Season = Season.Summer;
+  console.log(current);
+
+  current = Season.Autumn; // изменение значения
+
+  enum AnotherSeason { Winter=2, Spring, Summer=42, Autumn }; // {2: "Winter", 3: "Spring", 42: "Summer", 43: "Autumn", Winter: 2, Spring: 3, Summer: 42, Autumn: 43}
+
+  // можно получить строковое значение по номеру
+  console.log(AnotherSeason[42]); // Summer
+})();
