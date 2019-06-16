ef913071042125a5ba92d834d6d7d26f83b3d883
Vassily Krasnov <mykrasnov@gmail.com>
2019-06-16 17:07:15 +0300

    Add new line

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ const el = this.document.getElementById('content');
 class User {
   name: string;
   age: number;
+
   constructor(_name: string, _age: number) {
     this.name = _name;
     this.age = _age;
