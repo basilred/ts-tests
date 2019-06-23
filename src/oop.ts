e1080ff4effeb913008798fad4b9ef08c53b5265
Vassily Krasnov <mykrasnov@gmail.com>
2019-06-23 22:26:27 +0300

    Add constructor to class User

--- src/oop.ts
+++ src/oop.ts
@@ -5,18 +5,21 @@ export default function() {
   class User {
     id: number;
     name: string;
+
+    constructor(userId: number, userName: string) {
+      this.id = userId;
+      this.name = userName;
+    }
+
     getInfo(): string {
       return `id: ${this.id}, name: ${this.name}`;
     }
   }
 
-  const tom: User = new User();
-  tom.id = 1;
-  tom.name = 'Tom';
+  const tom: User = new User(1, 'Tom');
   console.log(tom.getInfo());
+  tom.id = 4;
 
-  const alice: User = new User();
-  alice.id = 2;
-  alice.name = 'Alice';
+  const alice: User = new User(2, 'Alice');
   console.log(alice.getInfo());
 };
