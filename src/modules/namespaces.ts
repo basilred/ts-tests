4a0f7eb51f8982f8376120f62eacfda5435fc594
Vassily Krasnov <mykrasnov@gmail.com>
2019-07-01 23:39:35 +0300

    Add interface, function & constant to the namespace

--- src/modules/namespaces.ts
+++ src/modules/namespaces.ts
@@ -1,13 +1,26 @@
+namespace Personnel {
+  export interface IUser {
+    displayInfo();
+  }
+
+  export class Employee {
+    constructor(public name: string) {}
+  }
+
+  export function work(emp: Employee): void {
+    console.log(`${emp.name} is working`);
+  }
+
+  export const defaultUser = { name: 'Kate' };
+}
+
 export default function() {
   // Пространства имён
   ((sectionName: string) => {
     console.log(`\n${sectionName}`);
 
-    namespace Personnel {
-      export class Employee {
-        constructor(public name: string) {}
-      }
-    }
+    const alice = new Personnel.Employee('Alice');
+    console.log(Personnel.work(alice));
 
   })('Пространства имён');
 };
