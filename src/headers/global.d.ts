5781384dfe93da47304ea3c96a5d5634d612e97e
Vassily Krasnov <mykrasnov@gmail.com>
2019-07-03 15:21:26 +0300

    Connect an array of objects

--- src/headers/global.d.ts
+++ src/headers/global.d.ts
@@ -1,2 +1,8 @@
 declare const globalVar: string;
 declare function display(): void;
+
+interface IPoint {
+  X: number;
+  Y: number;
+}
+declare const points: IPoint[];
--- src/headers/index.ts
+++ src/headers/index.ts
@@ -6,6 +6,10 @@ export default function() {
       static displayGlobalVar() {
         // console.log(globalVar);
         display();
+
+        for (let i = 0; i < points.length; i++ ) {
+          console.log(`Точка с координатами X=${points[i].X}, Y=${points[i].Y}`);
+        }
       }
     }
 
