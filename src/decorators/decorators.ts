5090159a56a4c0d4c73cd5f2a000f95332a46b8b
Vassily Krasnov <mykrasnov@gmail.com>
2019-07-05 23:58:25 +0300

    Add method parameters decorators

--- src/decorators/decorators.ts
+++ src/decorators/decorators.ts
@@ -107,5 +107,72 @@ export default function() {
 
     })('Параметры и выходной результат метода');
 
+
+    ((sectionName: string) => {
+      console.log(`\n${sectionName}`);
+
+      function logParameter(target: any, key: string, index: number) {
+        const metadataKey = `__log_${key}_parameters`;
+
+        if (Array.isArray(target[metadataKey])) {
+          target[metadataKey].push(index);
+        } else {
+          target[metadataKey] = [index];
+        }
+      }
+
+      function logMethod(target, key, descriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = function(...args: any[]) {
+          const metadataKey = `__log_${key}_parameters`;
+          const indices = target[metadataKey];
+
+          if (Array.isArray(indices)) {
+            for (let i = 0; i < args.length; i += 1) {
+              if (indices.indexOf(i) !== -1) {
+                const arg = args[i];
+                const argStr = JSON.stringify(arg) || arg.toString();
+                console.log(`${key} arg[${i}]: ${argStr}`);
+              }
+            }
+            const result = originalMethod.apply(this, args);
+
+            return result;
+          }
+          else {
+            const a = args.map(a => (JSON.stringify(a) || a.toString())).join();
+            const result = originalMethod.apply(this, args);
+            const r = JSON.stringify(result);
+            console.log(`Call: ${key}(${a}) => ${r}`);
+
+            return result;
+          }
+        }
+        return descriptor;
+      }
+
+      class User {
+        private name: string;
+        constructor(name: string) {
+          this.name = name;
+        }
+
+        @logMethod
+        setName(@logParameter name: string) {
+          this.name = name;
+        }
+
+        print():void {
+          console.log(this.name);
+        }
+      }
+
+      const tom = new User("Tom");
+      tom.setName("Bob");
+      tom.setName("Sam");
+
+    })('Декораторы параметров методов');
+
   })('Декораторы');
 }
