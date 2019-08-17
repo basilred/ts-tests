96974ae9c76b59c0af8532a2d7c56bc5db714c1b
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-17 17:04:49 +0300

    Create webpack config to run .ts files

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/app.js',
+  entry: './src/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
